5f9490d22e3903a4c5dbb94461b4a312e4b66696
Noah Kritz <noahkritz@gmail.com>
2019-03-17 01:57:05 -0400

    added bashrc / profile

---
+++ .bash_profile
@@ -0,0 +1,3 @@
+export PATH="$HOME/.cargo/bin:/opt/local/bin:/opt/local/sbin:$PATH"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+source ~/.bashrc
--- addconfigs.sh
+++ addconfigs.sh
@@ -3,4 +3,6 @@
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf 
 cp .radare2rc ~/.radare2rc
+cp .bashrc ~/.bashrc
+cp .bash_profile ~/.bash_profile
 tmux source ~/.tmux.conf
