820b0c19a24ea65130a8add754526cb1a878707a
Noah Kritz <noahkritz@gmail.com>
2019-04-29 16:22:19 -0400

    Added nerd-font install to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
+dir = $(pwd)
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-./addconfigs.sh
+git clone https://github.com/ryanoasis/nerd-fonts.git ~/Desktop/nerd-fonts && cd ~/Desktop/nerd-fonts && ./install.sh Hack
+cd dir && ./addconfigs.sh
 vim +PluginInstall
