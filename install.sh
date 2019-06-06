064fe9a563a9fdd201cb9b5c5461d9b457700908
Noah Kritz <noahkritz@gmail.com>
2019-06-06 15:12:14 -0400

    Updated install for zsh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-dir = $(pwd)
+dir=$(pwd)
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/ryanoasis/nerd-fonts.git ~/Desktop/nerd-fonts && cd ~/Desktop/nerd-fonts && ./install.sh Hack
-cd dir && ./addconfigs.sh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cd $dir && ./addconfigs.sh
 vim +PluginInstall
