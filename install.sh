044384fde841465bb773bfca2d95af810a53cb0f
Noah Kritz <kritz@battelle.org>
2019-06-26 10:09:30 -0400

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 dir=$(pwd)
+sudo apt install zsh curl vim tmux git -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/ryanoasis/nerd-fonts.git ~/Desktop/nerd-fonts && cd ~/Desktop/nerd-fonts && ./install.sh Hack
-sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cd $dir && ./addconfigs.sh
 vim +PluginInstall
