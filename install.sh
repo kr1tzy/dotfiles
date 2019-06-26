3a6696cdf9588835b9146fb1839dd9b1e753de1a
Noah Kritz <kritz@battelle.org>
2019-06-26 10:25:25 -0400

    sudo to .oh-my-zsh install

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo apt install zsh curl vim tmux git -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/ryanoasis/nerd-fonts.git ~/Desktop/nerd-fonts && cd ~/Desktop/nerd-fonts && ./install.sh Hack
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cd $dir && ./addconfigs.sh
 vim +PluginInstall
