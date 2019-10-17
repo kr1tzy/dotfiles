8e9e43a36cdc6b7fd9d582d2791becacda05225b
noah kritz <noahkritz@gmail.com>
2019-10-17 11:00:57 -0400

    Updated install script to only install the Hack Nerd Font; works on Linux, still need to test on Mac

--- addconfigs.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cp .vimrc ~/.vimrc
-cp .tmux.conf ~/.tmux.conf 
-cp .radare2rc ~/.radare2rc
-cp .zshrc ~/.zshrc
--- install.sh
+++ install.sh
@@ -4,14 +4,17 @@ dir=$(pwd)
 
 # OS Specific installs
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	sudo apt install zsh curl vim tmux git fonts-hack-ttf -y
+	sudo apt install zsh curl vim tmux git wget unzip -y
 	sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	cd $dir && sudo ./addconfigs.sh
+	wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf\?raw\=true -O ./Hack_Nerd_Font.ttf
+	mv ./Hack_Nerd_Font.ttf ~/.local/share/fonts/
+	cd $dir && add_configs
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	brew cask install caskroom/fonts/font-hack
+	brew tap homebrew/cask-fonts
+	brew cask install font-hack-nerd-font
 	brew install zsh
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	cd $dir && ./addconfigs.sh
+	add_configs
 else
 	echo "OS is not accounted for in install.sh"
 fi
@@ -20,3 +23,14 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 vim +PluginInstall
+
+echo && echo "[!] Install successful! Make sure to change the default font in your terminal to the Hack Nerd Font."
+
+# Adds the config files to the home directory
+add_configs () { 
+	cd $dir
+	cp .vimrc ~/.vimrc
+	cp .tmux.conf ~/.tmux.conf 
+	cp .radare2rc ~/.radare2rc
+	cp .zshrc ~/.zshrc
+}
