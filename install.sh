9a7b15cf4f5cc87436c78d858a3803676c37f852
Noah Kritz <nkritz@redlattice.com>
2022-05-11 14:51:48 -0400

    brew install to bash instead of ruby

--- install.sh
+++ install.sh
@@ -70,7 +70,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     which -s brew
     if [[ $? != 0 ]] ; then
         # Install Homebrew
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     else
         brew update
     fi
