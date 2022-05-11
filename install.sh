a2aa205bfb56d2a6dd24a640c9a9f61c2a2599a3
Noah Kritz <nkritz@Noahs-Mac-mini.bacon>
2022-05-11 12:18:38 -0400

    updated install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ CURRENT_DIR=$(pwd)
 
 # ----------------------------------------------------
 # --- Installs program if it's not available via brew.
-brew_check_and_install () {
+brew_install () {
     for program in "$@"; do
         if brew ls --versions $program > /dev/null; then
             echo "[*] $program found; skipping install!"
@@ -15,9 +15,9 @@ brew_check_and_install () {
 }
 
 
-# -----------------------------------------------------
-# --- These only install if they're not already present
-os_independent_installs () {
+# ---------------------------------------------
+# --- These only install if they're not present
+install () {
     # vim-plug
     if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
         curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &
@@ -25,7 +25,7 @@ os_independent_installs () {
         echo "[*] ~/.vim/autoload/plug.vim found; skipping install!"
     fi
 
-    # Tmux package manager
+    # tmux package manager
     if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &
     else
@@ -51,21 +51,20 @@ add_configs () {
 	cp .vimrc ~/.vimrc
 	cp .tmux.conf ~/.tmux.conf 
 	cp .zshrc ~/.zshrc
+	cp .zprofile ~/.zprofile
 }
 
 
-# OS Specific installs
+# ------------------------
+# --- OS Specific installs
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	echo "[*] Starting Linux install..."
 
     sudo apt install vim tmux zsh git curl wget unzip nodejs -y
     sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-	# Hack Nerd Font
-	wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf\?raw\=true -O ./Hack_Nerd_Font.ttf
-
-    echo "[*] Remember to install the 'Hack_Nerd_Font.ttf' to your system"
 	echo "[*] Copying config files to $HOME" && cd $CURRENT_DIR && add_configs
+    echo "[*] Click the 'Hack_Nerd_Font.ttf' to install it to your system"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	echo "[*] Starting Mac install..."
 
@@ -74,11 +73,10 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     else
         echo "[*] Tapping homebrew/cask-fonts"
         brew tap homebrew/cask-fonts
-        echo "[!] Make sure to change the default font in your terminal to Nerd Hack!"
     fi
 
-    programs=("font-hack-nerd-font --cask" "zsh" "tmux" "lsd" "bat")
-    brew_check_and_install "${programs[@]}"
+    programs=("font-hack-nerd-font --cask" "git" "tmux" "node")
+    brew_install "${programs[@]}"
 
 	if [ ! -d "$HOME/.oh-my-zsh" ]; then 
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -92,6 +90,6 @@ else
 	echo "[!] OS not accounted for in install.sh!"
 fi
 
-
-# OS Independent installs
-os_independent_installs && echo "[*] Done! :)"
+ 
+install && echo "[*] Done! :)"
+echo "[!] Make sure to change the default font in your terminal to Nerd Hack"
