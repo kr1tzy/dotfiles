5c0c8088b1d09536b2458b810708e1c64fd60c2c
Noah Kritz <nkritz@redlattice.com>
2022-05-11 15:10:58 -0400

    updated echo output for brew install

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 	echo "[*] Starting Mac install..."
     which -s brew
     if [[ $? != 0 ]] ; then
-        echo 'First install Homebrew: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' && exit
+        echo '[!] First install Homebrew: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' && exit
     else
         brew update
     fi
